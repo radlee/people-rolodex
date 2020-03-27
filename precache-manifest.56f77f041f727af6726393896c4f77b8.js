1aea0f5e60df7702280eb970f7b78a221fa143c0
radlee <prof.lolito@gmail.com>
2020-03-27 09:19:51 +0200

    Updates

---
+++ precache-manifest.56f77f041f727af6726393896c4f77b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3ccb0a4c6afa359f9df0e392cb5c06",
+    "url": "/people-rolodex/index.html"
+  },
+  {
+    "revision": "61e6e2adc9f2eb9d0eb4",
+    "url": "/people-rolodex/static/css/main.a0b644cb.chunk.css"
+  },
+  {
+    "revision": "1a23a1e5fb09e429e202",
+    "url": "/people-rolodex/static/js/2.290c8361.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/people-rolodex/static/js/2.290c8361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e6e2adc9f2eb9d0eb4",
+    "url": "/people-rolodex/static/js/main.7edb6989.chunk.js"
+  },
+  {
+    "revision": "fed46863a051b7333294",
+    "url": "/people-rolodex/static/js/runtime-main.047f4c5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/people-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
